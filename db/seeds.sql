add2c35d92dbf69499e719e239140ee8652d7f1f
Lucas Plumb <Lucasplumb@live.com>
2021-03-16 22:45:34 -0500

    add css file and worked on handels

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Guido", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("double junior whopper", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("big mac", true);
---
+++ server.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql")
+const util = require("util")
+const express = require('express');
+
+const app = express()
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
